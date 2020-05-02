c180b82a422e32dbaefee9e637f7bf4c650eb595
jk12 <jk@hades.ai>
2020-05-02 13:06:53 +0200

    added conv cgan

---
+++ CGAN-Pytorch-master/disc.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+def normal_init(m, mean, std):
+  if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+    m.weight.data.normal_(mean, std)
+    m.bias.data.zero_()
+
+class discriminator(nn.Module):
+  # initializers
+  def __init__(self, d=128, bs=2):
+    super(discriminator, self).__init__()
+    self.conv1_1 = nn.Conv2d(1, d, 16, 8, 1)
+    self.conv2 = nn.Conv2d(d, d * 2, 8, 4, 1)
+    self.conv2_bn = nn.BatchNorm2d(d * 2)
+    self.conv3 = nn.Conv2d(d * 2, d * 4, 4, 2, 1)
+    self.conv3_bn = nn.BatchNorm2d(d * 4)
+    self.conv4 = nn.Conv2d(d * 4, d * 8, 4, 2, 1)
+    self.conv4_bn = nn.BatchNorm2d(d * 8)
+    self.conv5 = nn.Conv2d(d * 8, 1, 4, 1, 0)
+    self.Lin = nn.Linear(16384*bs,bs)
+
+  # weight_init
+  def weight_init(self, mean, std):
+    for m in self._modules:
+      normal_init(self._modules[m], mean, std)
+
+  # forward method
+  # def forward(self, input):
+  def forward(self, input):
+    x = F.leaky_relu(self.conv1_1(input), 0.2)
+    x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+    x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+    x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2).flatten()
+    x = self.Lin(x)
+
+    return x
